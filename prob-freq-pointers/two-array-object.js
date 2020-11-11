6b6ae036f4e9b6abfafbcacc066078d7b4842738
Winnie Chou <winnie.chou.mb@gmail.com>
2020-11-11 17:50:26 -0600

    two array object

--- prob-freq-pointers/two-array-object.js
+++ prob-freq-pointers/two-array-object.js
@@ -1,3 +1,16 @@
-// add whatever parameters you deem necessary
-function twoArrayObject() {
+"use strict";
+
+/* Taking in two arrays (keys and values) and returning an object using the array as key value pairs */
+function twoArrayObject(keys, values) {
+    let keysToValues = {};
+
+    for(let i=0; i<keys.length; i++){
+        if(values[i] === undefined) {
+            keysToValues[keys[i]] = null;
+        } else {
+            keysToValues[keys[i]] = values[i];
+        }
+    }
+
+    return keysToValues;
 }
