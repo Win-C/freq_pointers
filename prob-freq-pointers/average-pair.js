efd733befa896965a27dee2a9cedc3b01ad3fadf
J. Sterling Bishop <jsterlingbishop@gmail.com>
2020-11-11 15:36:36 -0800

    implemented average-pair

--- prob-freq-pointers/average-pair.js
+++ prob-freq-pointers/average-pair.js
@@ -1,3 +1,20 @@
-// add whatever parameters you deem necessary
-function averagePair() {
+"use strict";
+/* Given a sorted aray of integers and a target average,
+return true if there are at least one pair of numbers that averages to the target. Otherwise return false. */
+function averagePair(integers, targetAverage) {
+    let leftPointer = 0;
+    let rightPointer = integers.length - 1;
+    while (leftPointer < rightPointer) {
+        let testAverage = (integers[leftPointer] + integers[rightPointer]) / 2;
+        if (testAverage === targetAverage) {
+            return true;
+        }
+        else if (testAverage > targetAverage) {
+            rightPointer--;
+        }
+        else {
+            leftPointer++;
+        }
+    }
+    return false;
 }
--- prob-freq-pointers/can-construct-word.js
+++ prob-freq-pointers/can-construct-word.js
@@ -1,33 +1,31 @@
 "use strict";
 
 /* Takes in two strings (word and letters) and returns true or false
-    True - if the word can be built by the letters
+    True - if the number of letters can be combined to create the word
     False - if not
     */
 
 function canConstructWord(word, letters) {
+    if (letters.length < word.length) return false;
+    if (word.length === 0) return true;
+
     let wordFreq = createFreqCounter(word);
     let lettersFreq = createFreqCounter(letters);
 
-    if(letters.length<word.length) return false;
-
-    if(word.length===0) return true;
-
-    for(let char of wordFreq.keys()){
-        if(!lettersFreq.has(char)) return false;
-        if(lettersFreq.get(char)<wordFreq.get(char)) return false;
+    for (let char of wordFreq.keys()) {
+        if (!lettersFreq.has(char)) return false;
+        if (lettersFreq.get(char) < wordFreq.get(char)) return false;
     }
     return true;
 }
 
 /* Helper function: creates a frequency counter from a string */
-function createFreqCounter(string){
+function createFreqCounter(string) {
     let frequencies = new Map();
 
-    for(let char of string){
+    for (let char of string) {
         let charCount = frequencies.get(char) || 0;
         frequencies.set(char, charCount + 1);
     }
-
     return frequencies;
 }
