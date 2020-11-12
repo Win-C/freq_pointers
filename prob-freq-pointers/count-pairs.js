f21e2f61f69e9821a0090943bc99be5fa58616d1
Winnie Chou <winnie.chou.mb@gmail.com>
2020-11-11 19:54:41 -0600

    step three

--- prob-freq-pointers/count-pairs.js
+++ prob-freq-pointers/count-pairs.js
@@ -1,3 +1,22 @@
-// add whatever parameters you deem necessary
-function countPairs() {
+"use strict";
+
+/* Takes in an array of integers and a target number.
+    Returns the number of pairs that sum to the target number */
+function countPairs(integers, targetSum) {
+    let count = 0;
+    let setIntegers = new Set(integers);
+
+    for(let num of integers){
+        let findValue = targetSum - num;
+        if(setIntegers.has(findValue)&&num!==findValue){
+            count++;
+            setIntegers.delete(findValue);
+            setIntegers.delete(num);
+        }
+    }
+
+    return count;
 }
+
+
+
