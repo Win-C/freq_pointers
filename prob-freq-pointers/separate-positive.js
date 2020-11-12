d524a47aff4cf5177153cdae11de38c7a006b3d5
Winnie Chou <winnie.chou.mb@gmail.com>
2020-11-11 18:46:38 -0600

    separate positives

--- prob-freq-pointers/separate-positive.js
+++ prob-freq-pointers/separate-positive.js
@@ -1,4 +1,23 @@
 "use strict";
-/* */
-function separatePositive() {
+
+/* Takes in an array of non-zero integers.
+    Returns the mutated array with positive integers to the left and negative integers to the right. Returned array might not be sorted. */
+function separatePositive(integers) {
+    let left = 0;
+    let right = integers.length-1;
+
+    while(left<right){
+        if((integers[left]<0)&&(integers[right]>0)){
+            [integers[left], integers[right]] = [integers[right], integers[left]];
+        } else {
+            if(integers[right]<0){
+                right--;
+            }
+            if(integers[left]>0){
+                left++;
+            }
+        }
+    }
+    return integers;
 }
+
